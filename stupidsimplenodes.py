6f3231e91a257772c67a29269433954650f00392
Project Soulstealer <six@6sphere.ru>
2023-07-28 21:28:38 +0300

    Added Numbers to SSN

--- stupidsimplenodes.py
+++ stupidsimplenodes.py
@@ -15,6 +15,40 @@ class SSN_Seed_INT:
     def seed(self, seed):
         return (int(seed), )
 
+class SSN_INT_INT:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+                    "required": {
+                        "INTEGER": ("INT", {"default": 0, "min": -0xffffffffffffffff, "max": 0xffffffffffffffff})
+                    }
+                }
+
+    RETURN_TYPES = ("INT",)
+    FUNCTION = "func"
+
+    CATEGORY = "StupidSimpleNodes"
+
+    def func(self, integer):
+        return (int(integer), )
+
+class SSN_FLOAT_FLOAT:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+                    "required": {
+                        "FLOATNUM": ("FLOAT", {"default": 0, "min": -0xffffffffffffffff, "max": 0xffffffffffffffff})
+                    }
+                }
+
+    RETURN_TYPES = ("FLOAT",)
+    FUNCTION = "func"
+
+    CATEGORY = "StupidSimpleNodes"
+
+    def func(self, floatnum):
+        return (float(floatnum), )
+
 class SSN_Math_Add_INT:
     @classmethod
     def INPUT_TYPES(cls):
@@ -35,5 +69,7 @@ class SSN_Math_Add_INT:
 
 NODE_CLASS_MAPPINGS = {
     "Stupid Simple Seed (INT)": SSN_Seed_INT,
-    "Stupid Simple Addition (INT)": SSN_Math_Add_INT
+    "Stupid Simple Addition (INT)": SSN_Math_Add_INT,
+    "Stupid Simple Number (INT)": SSN_INT_INT,
+    "Stupid Simple Number (FLOAT)": SSN_FLOAT_FLOAT
 }    
