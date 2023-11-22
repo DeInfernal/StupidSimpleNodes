0f55027b128809906e27e6eb89decf6db20e0a68
Project Soulstealer <six@6sphere.ru>
2023-11-22 17:24:52 +0300

    Added more simple nodes

--- stupidsimplenodes.py
+++ stupidsimplenodes.py
@@ -69,6 +69,26 @@ class SSN_Math_Add_INT:
     def add(self, var1, var2):
         return (int(var1)+int(var2), )
 
+
+class SSN_Math_Add_FLOAT:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+                    "required": {
+                        "var1": ("FLOAT", {"default": 0, "min": -0xffffffffffffffff, "max": 0xffffffffffffffff}),
+                        "var2": ("FLOAT", {"default": 0, "min": -0xffffffffffffffff, "max": 0xffffffffffffffff})
+                    }
+                }
+
+    RETURN_TYPES = ("FLOAT",)
+    FUNCTION = "add"
+
+    CATEGORY = "StupidSimpleNodes"
+
+    def add(self, var1, var2):
+        return (float(var1)+float(var2), )
+
+
 class SSN_SAMPLER:
     @classmethod
     def INPUT_TYPES(cls):
@@ -105,11 +125,51 @@ class SSN_SCHEDULER:
     def func(self, scheduler):
         return (scheduler, )
 
+
+class SSN_STRING:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+                    "required": {
+                        "string": ("STRING", {"multiline": False}),
+                    }
+                }
+
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "func"
+
+    CATEGORY = "StupidSimpleNodes"
+
+    def func(self, string):
+        return (string, )
+
+
+class SSN_TEXT:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+                    "required": {
+                        "text": ("STRING", {"multiline": True}),
+                    }
+                }
+
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "func"
+
+    CATEGORY = "StupidSimpleNodes"
+
+    def func(self, text):
+        return (text, )
+
+
 NODE_CLASS_MAPPINGS = {
     "Stupid Simple Seed (INT)": SSN_Seed_INT,
     "Stupid Simple Addition (INT)": SSN_Math_Add_INT,
+    "Stupid Simple Addition (FLOAT)": SSN_Math_Add_FLOAT,
     "Stupid Simple Number (INT)": SSN_INT_INT,
     "Stupid Simple Number (FLOAT)": SSN_FLOAT_FLOAT,
+    "Stupid Simple String": SSN_STRING,
+    "Stupid Simple Text": SSN_TEXT,
     "Stupid Simple Sampler": SSN_SAMPLER,
     "Stupid Simple Scheduler": SSN_SCHEDULER
 }    
